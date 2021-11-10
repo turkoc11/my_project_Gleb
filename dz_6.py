987277ec0a9785c86852b75be3d241dc9ca171bc
turkoc11 <turkovgleb@gmail.com>
2021-11-10 16:18:10 +0200

    second commit

--- dz_6.py
+++ dz_6.py
@@ -7,12 +7,22 @@ print(list_a)
 list_b=[random.randint(1,10) for _ in range(20)]
 
 print(list_b)
+count=0
+count1=0
+unique_b=[]
+unique_a=[]
+for i in list_a:
+    if i not in unique_a:
+        count=count+1
+        unique_a.append(i)
+for j in list_b:
+    if j not in unique_b:
+        count1=count1+1
+        unique_b.append(j)
 
-unique_a=list(set(list_a))
+print(unique_a, unique_b, count, count1)
 
-unique_b=list(set(list_b))
 
-print(f"Уникальные значения для а:{unique_a}", f"Уникальные значения для b:{unique_b}", len(unique_a),len(unique_b))
 
 
 
